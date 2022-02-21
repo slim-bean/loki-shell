ecf073386fa8dfa7bb2e43b2389a5bdc0b8c6877
Edward Welch <edward.welch@grafana.com>
2022-02-21 16:27:19 -0500

    updating bash to work the same as zsh

--- shell/loki-shell.bash
+++ shell/loki-shell.bash
@@ -8,13 +8,36 @@ else
     }
 fi
 
+_load_all_cmd(){
+  END=$(date --iso-8601=seconds)
+  for i in `seq 720 720 8640`
+  do
+    START=$(date -d "-$i hours" --iso-8601=seconds)
+    $HOME/.loki-shell/bin/logcli query "{job=\"shell\"}" --addr=$LOKI_URL --limit=50000 --batch=1000 --from=$START --to=$END -o raw --quiet
+    END=$START
+  done
+}
+
+_load_all() {
+  _load_all_cmd | _bufcmd
+}
+
 __fzf_history__() {
-  local output
-  output=$(
-    $HOME/.loki-shell/bin/logcli query "{job=\"shell\", host=\"$HOSTNAME\"}" --addr=$LOKI_URL --limit=50000 --batch=1000 --since=4380h -o raw --quiet | _bufcmd |
-      FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS +m " $(__fzfcmd) --query "$READLINE_LINE"
-      ) || return
-  READLINE_LINE=${output#*$'\t'}
+  local selected
+  if [[ -v LS_LOCAL ]]; then
+    # This command is just copied from fzf with the additional header I'll try to keep it updated...
+    selected=$(
+      builtin fc -lnr -2147483648 |
+        last_hist=$(HISTTIMEFORMAT='' builtin history 1) perl -n -l0 -e 'BEGIN { getc; $/ = "\n\t"; $HISTCMD = $ENV{last_hist} + 1 } s/^[ *]//; print $HISTCMD - $. . "\t$_" if !$seen{$_}++' |
+        FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --header 'LS_LOCAL is set, querying local history. unset LS_LOCAL to resume.' --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS +m --read0" $(__fzfcmd) --query "$READLINE_LINE"
+    ) || return
+  else
+    selected=$(
+      $HOME/.loki-shell/bin/logcli query "{job=\"shell\", host=\"$HOSTNAME\"}" --addr=$LOKI_URL --limit=50000 --batch=1000 --since=720h -o raw --quiet | _bufcmd |
+        FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --header 'ctrl-r to load ALL history, export LS_LOCAL=true for querying builtin history, export PRIVATE=true to not send commands to Loki.' --bind 'ctrl-r:reload(source $HOME/.loki-shell/shell/loki-shell.bash && _load_all)' $FZF_CTRL_R_OPTS +m " $(__fzfcmd) --query "$READLINE_LINE"
+    ) || return
+  fi
+  READLINE_LINE=${selected#*$'\t'}
   if [ -z "$READLINE_POINT" ]; then
     echo "$READLINE_LINE"
   else
@@ -24,7 +47,7 @@ __fzf_history__() {
 
 function _send_to_loki {
   if [[ -v PRIVATE ]]; then
-    echo "PRIVATE set, not sending to loki-shell"
+    echo "PRIVATE set, not sending to loki-shell. 'unset PRIVATE' to resume."
     return 0
   fi
 	(HISTTIMEFORMAT= builtin history 1 | sed 's/^ *\([0-9]*\)\** *//' |
